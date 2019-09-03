b22a7f672726465c80c1e6a604335436285fe84d
LeilaBeltran <Hermallony.beltran@gmail.com>
2019-09-03 10:42:31 -0500

    commit translation

--- translation.java
+++ translation.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
 
-codigo 
-=======
+/**
+ *
+ * @author leilaniigbeltran
+ */
+public class Translation {
 
-/* Code translation
--
-- here
--
-*/
-=======
-/*
-Aquí iria codigo*/
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String t, s;
+        
+        System.out.println("t: "); 
+            t=sc.next();
+        System.out.println("s: ");
+            s=sc.next();
+        String g = "";
+ //Array that compares both t and s
+       for(int x=t.length()-1;x>=0;x--){
+    g = (String) (g + t.charAt(x));
+       }  
+              if (s.equals(g)){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+
+
+    }
+    
+}
 
